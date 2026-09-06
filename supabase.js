7b5efc28fc2e97ac17a9cf534c19f5ec6752629e
sachin0664 <sachin80sachin80@gmail.com>
2026-09-07 00:34:39 +0530

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://lozluohksmjxbxixkbmq.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_R6LUj2dUcEwVL5Xuce-7Wg_8y4ZpnBm";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
